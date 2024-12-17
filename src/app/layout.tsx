de177392cd3db1b73ac47bc01249b37d9d798405
aabidshaikh001 <technicalsheikhshabb@gmail.com>
2024-12-17 13:44:19 +0530

    ok

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ export default function RootLayout({
       <head>
         <title>BharatPay - Leading B2B Digital Banking Services in India | Simplify Business Transactions</title>
         <meta name="description" content="Experience seamless B2B digital banking with BharatPay. Simplify business transactions, payments, and account management with our secure and efficient digital banking solutions. Grow your business with BharatPay today!" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       </head>
       <body>
       
